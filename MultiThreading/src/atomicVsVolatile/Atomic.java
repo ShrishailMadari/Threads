fd1f690aeb5f2aac535a908f7580f5db148f0fb2
shrishail madari <shrishailmadari@gmail.com>
2025-02-08 09:08:27 +0530

    add these files

---
+++ MultiThreading/src/atomicVsVolatile/Atomic.java
@@ -0,0 +1,22 @@
+package atomicVsVolatile;
+class SharedResource{
+    int counter;
+    public void setCounter(){
+        counter++; // three operations : read, update, assign
+//        this not atomic:
+//        System.out.println("increment counter: ");
+    }
+    public int getCounter(){
+        return counter;
+    }
+
+}
+public class Atomic {
+    public static void main(String[] args) {
+        SharedResource sharedResource = new SharedResource();
+        for (int i=0; i<400; i++){
+            sharedResource.setCounter();
+        }
+        System.out.println(sharedResource.getCounter());
+    }
+}
